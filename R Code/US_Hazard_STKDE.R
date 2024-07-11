25580a79f3e0bd22a18af375ce260cc9df0805f5
Etienne CAPRIOLI <etienne.caprioli@student-cs.fr>
2024-07-11 16:51:51 +0100

    Implementing te STKDE in R. Refining the evaluation mesh for STDE-PDE.

---
+++ R Code/US_Hazard_STKDE.R
@@ -0,0 +1,93 @@
+library(tidyverse)
+library(spatstat.geom)
+
+### Handling the storm data
+storm_data = read.csv("./Data/Prod_datasets/Storm_events_details_full_clean.csv")
+
+peril = "Hurricane"
+
+storm_analysis = storm_data %>%
+  filter(EVENT_CAT == peril) %>%
+  # filter(STATE != "HAWAII") %>%
+  # filter(STATE != "ALASKA") %>%
+  # filter(STATE != "PUERTO RICO") %>%
+  filter(DAMAGE_PROPERTY > 0) %>%
+  # filter(YEAR > 2004) %>%
+  # filter(YEAR < 2013) %>%
+  group_by(EPISODE_ID) %>%
+  summarise(YEAR = first(YEAR),
+            DAMAGE_PROPERTY = sum(DAMAGE_PROPERTY),
+            X_CART = mean(X_CART),
+            Y_CART = mean(Y_CART),
+            EPSIODE_NARRATIVE = first(EPISODE_NARRATIVE))
+
+storm_analysis = storm_analysis[complete.cases(storm_analysis %>% select(c("X_CART", "Y_CART"))),] 
+# Jittering
+storm_analysis = storm_analysis %>%
+  mutate(X_CART = X_CART + rnorm(nrow(storm_analysis), sd = 0.0003),
+         Y_CART = Y_CART + rnorm(nrow(storm_analysis), sd = 0.0003))
+
+years = unique(data$YEAR) %>% sort()
+
+# locations = storm_analysis %>% select(c("X_CART", "Y_CART", "YEAR")) %>% na.omit()
+# 
+# begin_year = min(locations$YEAR)
+# 
+# time_locations = locations$YEAR-begin_year
+# locations = matrix(data = c(locations$X_CART, locations$Y_CART), ncol = 2)
+
+
+### Creating the mesh
+
+boundaries_US = read.csv("./Data/US_map/boundaries_US_mesh.csv")
+boundaries_US = boundaries_US[-nrow(boundaries_US),]
+boundaries_US = boundaries_US[nrow(boundaries_US):1,]
+
+poly = list(x = boundaries_US$X_CART, y = boundaries_US$Y_CART)
+
+window = spatstat.geom::owin(poly = poly)
+
+inside_bool = spatstat.geom::inside.owin(x = storm_analysis$X_CART, y = storm_analysis$Y_CART, w = window)
+data = (storm_analysis %>% select(c("X_CART", "Y_CART", "YEAR")))[inside_bool, ]
+
+ppp_data = spatstat.geom::ppp(data$X_CART, data$Y_CART, window = window)
+
+# data_ppx = spatstat.geom::ppx(data, domain = list(spatial = window, temporal = range(time_locations)))
+
+st_intensity = sparr::spattemp.density(ppp_data, tt = data$YEAR, sres = 300)
+
+# Transforming into an intensity function
+lim_max = -Inf
+for (year in unique(data$YEAR)){
+  st_intensity$z[[as.character(year)]]$v = st_intensity$z[[as.character(year)]]$v * nrow(data)/ (max(years) - min(years))
+  maxi = max(log10(st_intensity$z[[as.character(year)]]$v), na.rm = T)
+  if (maxi > lim_max){
+    lim_max = maxi
+  }
+}
+print(lim_max)
+
+x_coords = st_intensity$z[[as.character(unique(data$YEAR)[1])]]$xcol
+y_coords = st_intensity$z[[as.character(unique(data$YEAR)[1])]]$yrow
+
+intensity_data = expand.grid(x = x_coords, y = y_coords)
+custom_colors <- c("blue", "green", "yellow", "red")
+
+years = st_intensity$tgrid
+# years = c(2015)
+
+for (year in years){
+  intensity_data$z = as.vector(st_intensity$z[[as.character(year)]]$v %>% t())
+  
+  p = ggplot() +
+    geom_tile(data = intensity_data, mapping = aes(x = x, y = y, fill = log10(z)), linewidth = 0) +
+    geom_path(data = boundaries_US, mapping = aes(x = X_CART, y = Y_CART)) +
+    # scale_fill_viridis_c(option = "plasma", na.value = "white", limits = c(-5, 7)) +
+    scale_fill_gradientn(colors = custom_colors, limits = c(-5, lim_max), na.value = "white")+
+    # scale_fill_gradient(palette = 'plasma', na.value = "white")+
+    labs(title = paste0("STKDE fo r",  peril, " at year ", year))+
+    theme_void()
+    
+  # plot(p)
+  ggsave(paste0("./R Code/R Data/STKDE/", peril,"/Plot_", tolower(peril),"_", year, ".png"), p, bg = "white", dpi = 600, width = 7, height = 5, unit = "in")
+}
--- R Code/US_hazards_STDE-PDE.R
+++ R Code/US_hazards_STDE-PDE.R
@@ -5,13 +5,16 @@ library(fdaPDE)
 ### Handling the storm data
 storm_data = read.csv("./Data/Prod_datasets/Storm_events_details_full_clean.csv")
 
+peril = "Tropical Storm"
+
 storm_analysis = storm_data %>%
-  filter(EVENT_CAT == "Thunderstorm") %>%
+  filter(EVENT_CAT == peril) %>%
   filter(STATE != "HAWAII") %>%
   filter(STATE != "ALASKA") %>%
   filter(STATE != "PUERTO RICO") %>%
   filter(DAMAGE_PROPERTY > 0) %>%
-  filter(YEAR > 2010) %>%
+  # filter(YEAR > 2004) %>%
+  # filter(YEAR < 2013) %>%
   group_by(EPISODE_ID) %>%
   summarise(YEAR = first(YEAR),
             DAMAGE_PROPERTY = sum(DAMAGE_PROPERTY),
@@ -36,7 +39,9 @@ storm_analysis = storm_analysis[complete.cases(storm_analysis %>% select(c("X_CA
 # locations_mesh = matrix(data = c(locations_mesh$X_CART, locations_mesh$Y_CART), ncol = 2)
 locations = storm_analysis %>% select(c("X_CART", "Y_CART", "YEAR")) %>% na.omit()
 
-time_locations = locations$YEAR-min(locations$YEAR)
+begin_year = min(locations$YEAR)
+
+time_locations = locations$YEAR-begin_year
 locations = matrix(data = c(locations$X_CART, locations$Y_CART), ncol = 2)
 
 
@@ -60,6 +65,7 @@ saveRDS(mesh, "./R Code/R Data/mesh_estimation.rds")
 data.mesh.eval = read.csv("./Data/US_map/evaluation_US_mesh.csv")
 eval.nodes = matrix(data = c(data.mesh.eval$X_CART, data.mesh.eval$Y_CART), ncol = 2)
 mesh.eval <- create.mesh.2D(nodes = rbind(boundary_nodes, eval.nodes), segments = boundary_segments)
+# mesh.eval = refine.mesh.2D(mesh, maximum_area = 0.00005)
 FEMbasis.eval <- create.FEM.basis(mesh.eval)
 plot(mesh.eval)
 
@@ -69,8 +75,8 @@ mesh_time = seq(range(time_locations)[1], range(time_locations)[2], by = 1)
 lambda_space = 10^seq(from = -4, to = -2, by = 1)
 lambda_time = 10^seq(from = -4, to = -2, by = 1)
 
-# lambda_space = 1e-1
-# lambda_time = 1e-1
+# lambda_space = 1e-2
+# lambda_time = 1e-2
 
 tryCatch({rm(storm_data)}, error = function(e){print(e)})
 
@@ -81,7 +87,7 @@ solution_STDEPDE <- DE.FEM.time(data = locations, data_time = time_locations, FE
                                 step_method = "Wolfe_Method", direction_method = "L-BFGS5",
                                 preprocess_method = "RightCV") 
 
-saveRDS(solution_STDEPDE, "./R Code/R Data/sol_thunderstorm.rds")
+saveRDS(solution_STDEPDE, paste0("./R Code/R Data/sol_", tolower(peril),".rds"))
 
 FEMfunction_STDEPDE <- FEM.time(solution_STDEPDE$g, mesh_time, FEMbasis, FLAG_PARABOLIC = F)
 
@@ -90,28 +96,39 @@ t <- mesh_time
 
 
 mean_sol_STDEPDE <- matrix(nrow = nrow(mesh.eval$nodes), ncol = length(t))
-
+lim_max = -Inf
 for (time_index in 1:length(t)) {
   
   evaluation_STDEPDE <- eval.FEM.time(FEM.time = FEMfunction_STDEPDE, locations = mesh.eval$nodes, time.instants = t[time_index])
   evaluation_STDEPDE <- exp(evaluation_STDEPDE)
   # evaluation_STDEPDE <- evaluation_STDEPDE/sum(evaluation_STDEPDE, na.rm = TRUE)
   
-  mean_sol_STDEPDE[,time_index] <- mapply(sum, mean_sol_STDEPDE[,time_index], evaluation_STDEPDE, na.rm = TRUE)
+  mean_sol_STDEPDE[,time_index] <- mapply(sum, mean_sol_STDEPDE[,time_index], evaluation_STDEPDE, na.rm = TRUE) * nrow(locations)/
+  maxi = max(log10(mean_sol_STDEPDE[,time_index]), na.rm = T)
+  if (maxi > lim_max){
+    lim_max = maxi
+  }
 }
+print(lim_max)
 
-saveRDS(mean_sol_STDEPDE, "./R Code/R Data/first_result_thunderstorm.rds")
-custom_colors <- c("blue", "green", "red")
+saveRDS(mean_sol_STDEPDE, paste0("./R Code/R Data/first_result_", tolower(peril), ".rds"))
+custom_colors <- c("blue", "green", "yellow", "red")
 
-time_index = 1
+
+t = 1:ncol(mean_sol_STDEPDE)
+
+intensity_data = data.frame(x = mesh.eval$nodes[,1], y = mesh.eval$nodes[,2], z = mean_sol_STDEPDE[,3])
+
 
 for (time_index in 1:length(t)){
-  p = ggplot(data.frame(x = mesh.eval$nodes[,1], y = mesh.eval$nodes[,2], z = log(mean_sol_STDEPDE[,time_index])))+
-    geom_point(mapping = aes(x = x, y=y, col = z))+
-    scale_color_gradientn(colors = custom_colors, limits = c(-5, 6))+ #
-    labs(title = paste0("STDE-PDE at year ",t[time_index]+1996))+
-    theme_minimal()
-  ggsave(paste0("./R Code/R Data/Plots_thunderstorm/intensity_", t[time_index]+1996, ".png"), plot = p, width = 10, height = 7, dpi = 300)
+  p = ggplot() +
+    geom_point(data = intensity_data, mapping = aes(x = x, y = y, col = log10(z)), size = 1) +
+    geom_path(data = boundaries_US, mapping = aes(x = X_CART, y = Y_CART)) +
+    scale_color_gradientn(colors = custom_colors, limits = c(-5, lim_max))+
+    labs(title = paste0("STKDE for ",  peril, " at year ", year))+
+    theme_void()
+  
+  ggsave(paste0("./R Code/R Data/STDE-PDE/Plots_", peril,"/plot_", tolower(peril), "_", t[time_index]+begin_year, ".png"), plot = p, width = 10, height = 7, dpi = 300)
   
 }
 
