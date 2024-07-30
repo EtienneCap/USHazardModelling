7131ba030cd60372d62d02d521b01db6102697ba
Etienne CAPRIOLI <etienne.caprioli@student-cs.fr>
2024-07-30 18:31:06 +0100

    Commit on gamma + GPD fit.

--- R Code/gamma_gpd_fit_test.R
+++ R Code/gamma_gpd_fit_test.R
@@ -13,7 +13,6 @@ storm_analysis_raw = storm_data %>%
   filter(STATE != "HAWAII") %>%
   filter(STATE != "ALASKA") %>%
   filter(STATE != "PUERTO RICO") %>%
-  filter(DAMAGE_PROPERTY > 0) %>%
   # filter(YEAR > 1996) %>%
   filter(YEAR < 2024) %>%
   group_by(EPISODE_ID) %>%
@@ -21,11 +20,12 @@ storm_analysis_raw = storm_data %>%
             EVENT_CAT = first(EVENT_CAT),
             STATE = first(STATE),
             STATE_FIPS = first(STATE_FIPS),
-            DAMAGE_PROPERTY = sum(DISCOUNT_DAMAGE_PROPERTY, na.rm = T),
+            DAMAGE = sum(DISCOUNT_DAMAGE_CROPS, na.rm = T),
             X_CART = mean(X_CART, na.rm = T),
             Y_CART = mean(Y_CART, na.rm = T),
             BEGIN_DATE_TIME = first(BEGIN_DATE_TIME),
-            EPSIODE_NARRATIVE = first(EPISODE_NARRATIVE))
+            EPSIODE_NARRATIVE = first(EPISODE_NARRATIVE)) %>%
+  filter(DAMAGE > 0)
 
 storm_analysis_raw = storm_analysis_raw[complete.cases(storm_analysis_raw %>% dplyr::select(c("EPISODE_ID", "X_CART", "Y_CART"))),]
 
@@ -37,6 +37,7 @@ GDP_state = GDP_state %>% pivot_longer(cols = starts_with("X"), names_to = "YEAR
 GDP_state = GDP_state %>%
   mutate(YEAR = as.numeric(substr(YEAR, 2,5))) %>%
   mutate(GeoFips = as.integer(GeoFips/1000))
+
 
 HPI_state = read.csv("./Data/mapping_tables/US_HPI_by_state.csv")
 HPI_state = HPI_state %>%
@@ -71,59 +72,69 @@ storm_analysis = storm_analysis_raw %>%
   left_join(HPI_state %>% dplyr::select(c("yr", "period", "place_name", "HPI")), by = c("YEAR" = "yr", "quart" = "period", "STATE" = "place_name")) %>%
   left_join(US_regions %>% dplyr::select(c("STATE", "REGION", "CLIM_REGION", "CUSTOM_REGION")), by = "STATE") %>%
   mutate(REGION = factor(REGION)) %>%
-  mutate(DAMAGE_LOG = log10(DAMAGE_PROPERTY)) %>%
-  mutate(DAMAGE_LN = log(DAMAGE_PROPERTY)) %>%
+  mutate(DAMAGE_LOG = log10(DAMAGE)) %>%
+  mutate(DAMAGE_LN = log(DAMAGE)) %>%
   mutate(STATE_FIPS = factor(STATE_FIPS)) %>%
   # group_by(REGION, EVENT_CAT) %>%
-  mutate(DAMAGE_SCALED = scale(DAMAGE_PROPERTY, center = F)[,1]) %>%
-  mutate(thresh_log = quantile(DAMAGE_LOG, tau),
-         thresh = quantile(DAMAGE_PROPERTY, tau),
-         thresh_ln = quantile(DAMAGE_LN, tau),
-         n = n()) %>%
+  mutate(DAMAGE_SCALED = scale(DAMAGE, center = F)[,1]) %>%
+  mutate(n = n()) %>%
   filter(!is.nan(DAMAGE_SCALED)) %>%
   # ungroup() %>%
-  mutate(DAMAGE_BOX_COX = sae::bxcx(DAMAGE_PROPERTY/sd(DAMAGE_PROPERTY), lambda_bxcx)) %>%
+  mutate(DAMAGE_BOX_COX = sae::bxcx(DAMAGE, lambda_bxcx)) %>%
   mutate(min_BXCX = min(DAMAGE_BOX_COX)) %>%
   mutate(DAMAGE_BOX_COX = DAMAGE_BOX_COX - min(DAMAGE_BOX_COX)) %>%
-  mutate(MEAN = mean(DAMAGE_PROPERTY)) %>%
-  mutate(MED = median(DAMAGE_PROPERTY)) %>%
+  mutate(MEAN = mean(DAMAGE)) %>%
+  mutate(MED = median(DAMAGE)) %>%
   mutate(BEGIN_DATE_TIME = as.Date(BEGIN_DATE_TIME)) %>%
-  mutate(SCALER = sqrt(mean(DAMAGE_PROPERTY^2))) %>%
+  mutate(SCALER = sqrt(mean(DAMAGE^2))) %>%
   filter(!is.na(REGION)) %>%
-  filter(DAMAGE_PROPERTY > 500) %>%
-  mutate(YEAR = YEAR - min(YEAR))
-
-### ASSESSING TIME IMPACT ###
-
-data_quantile = storm_analysis_raw %>%
-  group_by(YEAR) %>%
-  summarise(q = quantile(DAMAGE_PROPERTY, 0.9))
-
-quant_reg = rq(DAMAGE_BOX_COX ~ YEAR + REGION , data = storm_analysis)
-summary(quant_reg)
-
-
-### DAMAGE MODELLING ###
-
+  filter(DAMAGE > 500) %>%
+  mutate(YEAR = YEAR - min(YEAR)) %>%
+  mutate(PERIOD = YEAR %/% 10)
 
 par(mfrow = c(1,1))
 storm_analysis %>% pull(DAMAGE_BOX_COX) %>% hist(breaks = 30)
 storm_analysis %>% pull(DAMAGE_BOX_COX) %>% VGAM::meplot()
 
-storm_analysis %>% group_by(CLIM_REGION) %>% summarise(n = n())
+### ASSESSING TIME IMPACT ###
 
-sub_storm_analysis = storm_analysis %>% filter(CLIM_REGION == "Southwest") 
-thresh_quant = sub_storm_analysis %>% pull(DAMAGE_BOX_COX) %>%
-  quantile(0.95)
+quant_reg = rq(DAMAGE ~ YEAR + CUSTOM_REGION , data = storm_analysis, tau = 0.80)
+summary(quant_reg)
 
-thresh_quant = thresh_quant[[1]]
+plot(quant_reg$model$YEAR, quant_reg$fitted.values %>% log10())
+
+storm_analysis["THRESH_QUANT"] = quant_reg$fitted.values
+
+### DAMAGE MODELLING ###
+
+storm_analysis %>% group_by(CUSTOM_REGION) %>% summarise(n = n())
+
+## Threshold selection
+
+par(mfrow = c(3,2))
+
+thresh_list = c("North/Ohio Valley" = 10, "South" = 11, "Northwest" = 9, "Southeast" = 10, "Northeast" = 8, "West" = 11)
+
+for (reg in storm_analysis %>% pull(CUSTOM_REGION) %>% unique()){
+  storm_analysis %>% filter(CUSTOM_REGION == reg) %>% pull(DAMAGE_BOX_COX) %>% evmix::mrlplot(main = paste0("Region: ", reg), legend.loc = NULL, try.thresh = c(thresh_list[[reg]]))
+}
+
+sub_storm_analysis = storm_analysis %>%
+  # filter(PERIOD == 2) %>%
+  filter(CUSTOM_REGION == "North/Ohio Valley")
+
+# par(mfrow = c(3, 1))
+# storm_analysis %>% filter(PERIOD == 0) %>% pull(DAMAGE_LN)%>% hist(breaks = 30)
+# storm_analysis %>% filter(PERIOD == 1) %>% pull(DAMAGE_LN) %>% hist(breaks = 30)
+# storm_analysis %>% filter(PERIOD == 2) %>% pull(DAMAGE_LN) %>% hist(breaks = 30)
 
 
 res_gamma_GPD = sub_storm_analysis %>%
   pull(DAMAGE_BOX_COX) %>%
   fgammagpdcon(phiu = F)
   # fgammagpdcon(useq = 7, fixedu = T)
-  # fmgammagpdcon(M = 2, phiu = F)
+  # fmgammagpdcon(M = 2, phiu = F) 
+  # fgammagpdcon(useq = storm_analysis$THRESH_QUANT, fixedu = TRUE)
 
 res_gamma_GPD[-(1:2)]
 
@@ -135,7 +146,22 @@ evmix.diag(res_gamma_GPD, N = 250, upperfocus = F)
 evmix.diag(res_gamma_GPD, N = 250, upperfocus = T)
 par(mfrow = c(1,1))
 
-## Comparison with a gamm distribution
+
+test = sub_storm_analysis %>% filter(DAMAGE_BOX_COX > res_gamma_GPD$u)
+
+lin_model = lm(DAMAGE_BOX_COX ~ YEAR, data = sub_storm_analysis)
+summary(lin_model)
+
+
+
+
+densplot(res_gamma_GPD)
+
+## Comparison with a gamma fit
+
+gamma_fit = gamlss::gamlss(DAMAGE_BOX_COX ~ YEAR + CLIM_REGION, sigma.formula = ~ YEAR + CLIM_REGION, data = storm_analysis, family = gamlss.dist::GA())
+
+summary(gamma_fit)
 
 gamma_fit = sub_storm_analysis %>% pull(DAMAGE_BOX_COX) %>% fitdistrplus::fitdist(distr = "gamma", method = "mle")
 
@@ -153,6 +179,7 @@ custom_colors <- c("blue", "green", "red")
 ggplot() +
   geom_path(data = boundaries_US, mapping = aes(x = X_CART, y = Y_CART))+
   geom_point(data = storm_analysis, mapping = aes(x = X_CART, y = Y_CART, col = DAMAGE_LOG)) +
-  scale_color_gradientn(colors = custom_colors)
+  scale_color_gradientn(colors = custom_colors)+
+  theme_minimal()
 
 
