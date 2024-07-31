161811dfa84800f7434dca05b73bf9b3a702501d
Etienne CAPRIOLI <etienne.caprioli@student-cs.fr>
2024-07-31 16:18:38 +0100

    implementing homemade truncated gamma fitting with covariate.

--- R Code/gamma_gpd_fit_test.R
+++ R Code/gamma_gpd_fit_test.R
@@ -20,7 +20,7 @@ storm_analysis_raw = storm_data %>%
             EVENT_CAT = first(EVENT_CAT),
             STATE = first(STATE),
             STATE_FIPS = first(STATE_FIPS),
-            DAMAGE = sum(DISCOUNT_DAMAGE_CROPS, na.rm = T),
+            DAMAGE = sum(DISCOUNT_DAMAGE_PROPERTY, na.rm = T),
             X_CART = mean(X_CART, na.rm = T),
             Y_CART = mean(Y_CART, na.rm = T),
             BEGIN_DATE_TIME = first(BEGIN_DATE_TIME),
@@ -111,13 +111,163 @@ storm_analysis %>% group_by(CUSTOM_REGION) %>% summarise(n = n())
 
 ## Threshold selection
 
+regions = storm_analysis %>% pull(CUSTOM_REGION) %>% unique()
+
 par(mfrow = c(3,2))
 
-thresh_list = c("North/Ohio Valley" = 10, "South" = 11, "Northwest" = 9, "Southeast" = 10, "Northeast" = 8, "West" = 11)
+thresh_list = c("North - Ohio Valley" = 12, "South" = 13, "Northwest" = 8.5, "Southeast" = 10, "Northeast" = 10, "West" = 8)
 
-for (reg in storm_analysis %>% pull(CUSTOM_REGION) %>% unique()){
+for (reg in regions){
   storm_analysis %>% filter(CUSTOM_REGION == reg) %>% pull(DAMAGE_BOX_COX) %>% evmix::mrlplot(main = paste0("Region: ", reg), legend.loc = NULL, try.thresh = c(thresh_list[[reg]]))
 }
+
+## Bulk fitting (gamma distribution)
+
+
+# gamma_fits = rep(NA, length(thresh_list))
+# names(gamma_fits) = names(thresh_list)
+par(mfrow = c(3,2))
+for (reg in regions){
+  data = storm_analysis %>%
+    filter(CUSTOM_REGION == reg) %>%
+    filter(DAMAGE_BOX_COX <= thresh_list[[reg]])
+  
+  gamma_fit = gamlss::gamlss(DAMAGE_BOX_COX ~ YEAR, sigma.formula = ~ YEAR, data = data, family = gamlss.dist::GA())
+  print("######################################")
+  print(reg)
+  print(paste0("Number of rows: ", data %>% nrow()))
+  summary(gamma_fit)
+  plot(gamma_fit, main = paste0("Region: ", reg))
+  print("######################################")
+  saveRDS(gamma_fit, file = paste0("./R Code/Result_gamma_fit/", reg, ".rds"))
+}
+
+## Bulk fitting (truncated gamma distribution)
+alpha = 0.05
+for (reg in regions){
+  data_filtered = storm_analysis %>%
+    filter(CUSTOM_REGION == reg) %>%
+    filter(DAMAGE_BOX_COX <= thresh_list[[reg]])
+  
+  
+  # Define the truncated gamma density function
+  # dtruncgamma <- function(x, shape, rate, a = 0, b = thresh_list[[reg]]) {
+  #   dat = data.frame(x = x, shape = shape, rate = rate)
+  #   n = nrow(dat)
+  #   sapply(1:n, function(i){
+  #     if (dat$x[i] < a || dat$x[i] > b) {
+  #       return(-Inf)
+  #     }
+  #     else{
+  #       return(truncdist::dtrunc(dat$x[i], "gamma", shape = dat$shape[i], rate = dat$rate[i], a = a, b = b))
+  #     }
+  #   })
+  # }
+  
+  dtruncgamma_log = function(x, shape, rate, upper.bound = thresh_list[[reg]]){
+    int.const = pgamma(upper.bound, shape = shape, rate = rate, log.p = T)
+    lik = dgamma(x, shape = shape, rate = rate, log = T) - int.const
+    lik[x > upper.bound | x< 0] = -Inf
+    return(lik)
+  }
+  
+  loglik_truncgamma_cov <- function(params, data = data_filtered$DAMAGE_BOX_COX, time = data_filtered$YEAR) {
+    alpha_0 <- params[1]
+    alpha_1 <- params[2]
+    beta_0 <- params[3]
+    beta_1 <- params[4]
+    
+    shape <- exp(alpha_0 + alpha_1 * time)
+    rate <- exp(beta_0 + beta_1 * time)
+    
+    sum(dtruncgamma_log(data, shape = shape, rate = rate))
+  }
+  
+  init_params <- c(alpha_0 = 0.5, alpha_1 = 0.02, beta_0 = 0, beta_1 = 0.02)
+  
+  # Fit the truncated gamma distribution using maximum likelihood estimation
+  fit <- optim(init_params, loglik_truncgamma_cov, control = list(fnscale = -1, maxit = 1e5))
+  
+  hessian = numDeriv::hessian(function(x){loglik_truncgamma_cov(x)}, fit$par)
+  cov_matrix <- solve(-hessian*data_filtered %>% nrow())
+  
+  std_errors <- sqrt(diag(cov_matrix))
+  
+  # Confidence intervals
+  conf_intervals <- data.frame(
+    Estimate = fit$par,
+    lower.bound = fit$par + qnorm(alpha/2) * std_errors,
+    upper.bound = fit$par + qnorm(1-alpha/2) * std_errors)
+  
+  conf_intervals["signif"] = conf_intervals$`lower.bound` * conf_intervals$`upper.bound` > 0
+  
+  
+  
+  print("######################################")
+  print(reg)
+  print(paste0("Number of rows: ", data_filtered %>% nrow()))
+  print(fit)
+  print(conf_intervals)
+  print("######################################")
+  saveRDS(fit, file = paste0("./R Code/Result_gamma_fit/", reg, ".rds"))
+}
+
+# Fit assessement
+
+par(mfrow = c(3,2))
+for (reg in regions){
+  data_filtered = storm_analysis %>%
+    filter(CUSTOM_REGION == reg) %>%
+    filter(DAMAGE_BOX_COX <= thresh_list[[reg]])
+  
+  hist(data_filtered %>% pull(DAMAGE_BOX_COX), freq = F)
+  
+  fit = readRDS(paste0("./R Code/Result_gamma_fit/", reg, ".rds"))
+  
+  shape <- exp(fit$par[["alpha_0"]])
+  rate <- exp(fit$par[["beta_0"]])
+  
+  r = range(data_filtered %>% pull(DAMAGE_BOX_COX))
+  x = seq(r[1], r[2], length.out = 250)
+  lines(x,dgamma(x, shape = shape, rate = rate), col = 'red')
+}
+
+
+
+## Tail fitting (Generalised Pareto distribution)
+par(mfrow = c(3,2))
+for (reg in regions){
+  data = storm_analysis %>%
+    filter(CUSTOM_REGION == reg)
+  
+  GPD_fit = extRemes::fevd(x = data$DAMAGE_BOX_COX, 
+                               data = data.frame(data),
+                               threshold = thresh_list[[reg]],
+                               type = "GP", 
+                               scale.fun = ~ YEAR,
+                               shape.fun = ~ 1,
+                               method = "MLE")
+  print("######################################")
+  print(reg)
+  print(paste0("Number of rows: ", data %>% nrow()))
+  summary(GPD_fit)
+  print(ci.fevd(GPD_fit, type = "parameter", alpha = 0.05))
+  
+  # plot(GPD_fit, type = 'probprob')
+  plot(GPD_fit, type = 'qq', main = paste0("Region: ", reg))
+  print("######################################")
+  saveRDS(GPD_fit, file = paste0("./R Code/Result_GPD_fit/", reg, ".rds"))
+  
+}
+
+
+
+
+
+
+
+
+
 
 sub_storm_analysis = storm_analysis %>%
   # filter(PERIOD == 2) %>%
